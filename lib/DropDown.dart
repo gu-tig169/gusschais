fdee79aac60f30b83fa3dcf3eb3e8acfb36fe488
imschafhauser <gusschais@student.gu.se>
2020-11-27 15:11:46 +0100

    Steg 2 än så länge, steg 3 ej påbörjat ännu

---
+++ lib/DropDown.dart
@@ -0,0 +1,16 @@
+class DropDown {
+  static const String all = 'all';
+  static const String done = 'done';
+  static const String undone = 'undone';
+
+  static const List<String> choices = <String>[
+    all,
+    done,
+    undone,
+    
+  ];
+}
+
+class Filter {
+  static String show = 'all';
+}
